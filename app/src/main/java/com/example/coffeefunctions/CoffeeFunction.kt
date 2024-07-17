bdee5a57cdb0edf19265ce4b5a7a7327b51cd884
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-07-17 17:52:00 +0200

    add some try with functions

--- app/src/main/java/com/example/coffeefunctions/CoffeeFunction.kt
+++ app/src/main/java/com/example/coffeefunctions/CoffeeFunction.kt
@@ -1,20 +1,35 @@
 package com.example.coffeefunctions
 
 fun main(){
-    // Chiamate
-    makeCoffee(sugarCount = 1, name = "Giacomo")
-    println("Lavoro due ore")
-    makeCoffee(sugarCount = 3, name = "Ciccio")
-    makeCoffee(sugarCount = 0, name = "Cacca")
+    var myResult = add(num1 = 1, num2 = 2)
+    println("Il risultato della somma è: $myResult")
+
+    askDetails()
+}
+
+fun add(num1: Int, num2: Int): Int {
+    val result = num1 + num2
+    return result
 }
 
 // Dichiarazione
-fun makeCoffee(sugarCount : Int, name : String){
-    if (sugarCount == 1){
+fun makeCoffee(sugarCount: Int, name: String) {
+    if (sugarCount == 1) {
         println("Ecco il caffè con $sugarCount zucchero per $name")
     } else {
         println("Ecco il caffè con $sugarCount zuccheri per $name")
     }
     println("Ecco l'acqua")
     println("Mescola le due cose")
+}
+
+fun askDetails() {
+    println("Per chi è?")
+    val name = readln()
+    println("Quanto zucchero?")
+    val sugarStr = readln()
+    val sugar = sugarStr.toInt()
+    // Chiamate
+    makeCoffee(sugarCount = sugar, name = name)
+    println("Lavoro due ore")
 }
