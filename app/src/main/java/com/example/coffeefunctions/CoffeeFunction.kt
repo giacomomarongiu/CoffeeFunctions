608dd7499fa4f8405fc4aba22be1a154ec58a3aa
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-07-17 15:01:25 +0200

    Some try with functions

---
+++ app/src/main/java/com/example/coffeefunctions/CoffeeFunction.kt
@@ -0,0 +1,18 @@
+package com.example.coffeefunctions
+
+fun main(){
+    //Call
+makeCoffee()
+    println("Lavoro due ore")
+    makeCoffee()
+
+}
+
+
+//Declare
+fun makeCoffee(){
+    println("Ecco il caffè")
+    println("Ecco l'acqua")
+    println("Mescola le due cose")
+
+}
