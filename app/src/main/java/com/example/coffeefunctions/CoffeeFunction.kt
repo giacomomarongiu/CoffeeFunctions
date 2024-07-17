0f3400bbfb2fb373d78e8f4fd7e789fe6b085b72
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-07-17 15:14:21 +0200

    Some try with functions

--- app/src/main/java/com/example/coffeefunctions/CoffeeFunction.kt
+++ app/src/main/java/com/example/coffeefunctions/CoffeeFunction.kt
@@ -1,18 +1,20 @@
 package com.example.coffeefunctions
 
 fun main(){
-    //Call
-makeCoffee()
+    // Chiamate
+    makeCoffee(sugarCount = 1, name = "Giacomo")
     println("Lavoro due ore")
-    makeCoffee()
-
+    makeCoffee(sugarCount = 3, name = "Ciccio")
+    makeCoffee(sugarCount = 0, name = "Cacca")
 }
 
-
-//Declare
-fun makeCoffee(){
-    println("Ecco il caffè")
+// Dichiarazione
+fun makeCoffee(sugarCount : Int, name : String){
+    if (sugarCount == 1){
+        println("Ecco il caffè con $sugarCount zucchero per $name")
+    } else {
+        println("Ecco il caffè con $sugarCount zuccheri per $name")
+    }
     println("Ecco l'acqua")
     println("Mescola le due cose")
-
 }
